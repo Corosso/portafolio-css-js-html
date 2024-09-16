671da6f6493be9e1aebab0e5c09fa79ce0716556
Federico <fredolds180@gmail.com>
2024-09-16 14:02:34 -0500

    Actualizacion, se agrego seccion about me y seccion de tech stack

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+const descriptionText = document.getElementById('description-text');
+const langToggle = document.getElementById('lang-toggle');
+let currentLang = 'es'; // Initial language
+
+fetch('lang.json')
+  .then(response => response.json())
+  .then(data => {
+    const langData = data; // Define langData here
+    descriptionText.textContent = langData[currentLang].description;
+    langToggle.textContent = currentLang === 'es' ? 'English' : 'Español';
+
+    langToggle.addEventListener('click', () => {
+      if (currentLang === 'es') {
+        descriptionText.textContent = langData['en'].description;
+        langToggle.textContent = 'Español';
+        currentLang = 'en';
+      } else {
+        descriptionText.textContent = langData['es'].description;
+        langToggle.textContent = 'English';
+        currentLang = 'es';
+      }
+      const titleText = document.querySelector('h2');
+      titleText.textContent = langData[currentLang].title;
+    });
+  })
+  .catch(error => console.error('Error loading JSON file:', error));
